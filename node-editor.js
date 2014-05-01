f45ded9f747a875ed8b4c5e5d3a99b37e53c78a3
Vytautas Šaltenis <vytas@rtfb.lt>
2014-05-01 12:36:18 +0300

    Paste code for HookCollection
    I can't find a way to make Markdown.Editor.js see the HookCollection
    from Markdown.Converter.js, so this will have to do for now.

--- node-editor.js
+++ node-editor.js
@@ -1,8 +1,38 @@
 var pagedown = require("pagedown");
 global.window.Markdown = pagedown;
+global.window.Markdown.HookCollection = HookCollection;
 
 var editor = require("./Markdown.Editor");
 
 exports.getPagedownEditor = function() {
     return new Markdown.Editor(pagedown.getSanitizingConverter());
 }
+
+function identity(x) { return x; }
+function returnFalse(x) { return false; }
+function HookCollection() { }
+
+HookCollection.prototype = {
+
+    chain: function (hookname, func) {
+        var original = this[hookname];
+        if (!original)
+            throw new Error("unknown hook " + hookname);
+
+        if (original === identity)
+            this[hookname] = func;
+        else
+            this[hookname] = function (x) { return func(original(x)); }
+    },
+    set: function (hookname, func) {
+        if (!this[hookname])
+            throw new Error("unknown hook " + hookname);
+        this[hookname] = func;
+    },
+    addNoop: function (hookname) {
+        this[hookname] = identity;
+    },
+    addFalse: function (hookname) {
+        this[hookname] = returnFalse;
+    }
+};
